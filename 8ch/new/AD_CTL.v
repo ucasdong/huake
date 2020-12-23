3770f840bcacb209b445e94ef5287273deda9556
unknown <dlk254566110@163.com>
2020-12-23 19:11:40 +0800

    1

---
+++ 8ch/new/AD_CTL.v
@@ -0,0 +1,93 @@
+//根据鉴相频率及上位机配置的信息产生AD采样频率控制信号 ======AD_START
+module AD_CTL(
+	input				clk				,
+	input				rst_n			,
+	input          Phase_valid ,
+	input          reg_ad_enble,
+		
+	input		[15:0]	reg_freq1		,
+	input		[15:0]	reg_freq2		,
+	//input		[15:0]	reg_freq3		,
+	//input		[15:0]	reg_freq4		,
+	
+	input		[31:0]	Phase_cnt_out	,
+	
+	output	reg [4:1]	ad_start
+);
+
+reg                  tans_latch = 'b0;
+always@(posedge clk)
+  if(!reg_ad_enble)
+	   tans_latch <= 'b0;
+  else if(Phase_valid)
+     tans_latch  <= 'b1;
+  else
+     tans_latch  <= tans_latch;
+
+reg			[31:0]	freq1_cnt =1'b0;
+reg			[31:0]	freq2_cnt =1'b0;
+reg			[31:0]	freq3_cnt =1'b0;
+reg			[31:0]	freq4_cnt =1'b0;
+
+wire		[35:0]	Phase_cnt;	
+
+assign		Phase_cnt = {Phase_cnt_out[31:9],9'b0} * 32'd10;
+always@(posedge clk,negedge rst_n)
+	if(!rst_n) begin
+			freq1_cnt <=1'b0;
+			freq2_cnt <=1'b0;
+			freq3_cnt <=1'b0;
+			freq4_cnt <=1'b0;	end
+	else if(tans_latch)
+   	begin
+		if((freq1_cnt * reg_freq1 >= Phase_cnt) || Phase_valid)
+			freq1_cnt <= 1'b0;
+		else
+			freq1_cnt <= freq1_cnt + 1'b1;
+		if((freq2_cnt * reg_freq2 >= Phase_cnt) || Phase_valid)
+			freq2_cnt <= 1'b0;
+		else
+			freq2_cnt <= freq2_cnt + 1'b1;
+		/*if((freq3_cnt * reg_freq3 >= Phase_cnt)|| Phase_valid)
+			freq3_cnt <= 1'b0;
+		else
+			freq3_cnt <= freq3_cnt + 1'b1;
+		if((freq4_cnt * reg_freq4 >= Phase_cnt) ||Phase_valid)
+			freq4_cnt <= 1'b0;
+		else
+			freq4_cnt <= freq4_cnt + 1'b1;*/
+	end	
+	   else begin
+			freq1_cnt <=1'b0;
+			freq2_cnt <=1'b0;
+			freq3_cnt <=1'b0;
+			freq4_cnt <=1'b0;	end
+		
+		
+		
+		
+		
+always@(posedge clk) begin
+		if(freq1_cnt == 32'd3 || freq1_cnt == 32'd4)
+			ad_start[1]	<= 1'b1;
+		else
+			ad_start[1]	<= 1'b0;
+			
+		if(freq2_cnt == 32'd3 || freq2_cnt == 32'd4)
+			ad_start[2]	<= 1'b1;
+		else
+			ad_start[2]	<= 1'b0;
+		
+		if(freq3_cnt == 32'd3 || freq3_cnt == 32'd4)
+			ad_start[3]	<= 1'b1;
+		else
+			ad_start[3]	<= 1'b0;
+			
+		if(freq4_cnt == 32'd255 || freq4_cnt == 32'd256)
+			ad_start[4]	<= 1'b1;
+		else
+			ad_start[4]	<= 1'b0;
+	end	
+		
+		
+endmodule
---
+++ 8ch/new/Phase_detector.v
@@ -0,0 +1,91 @@
+
+//������Ƶģ��
+module Phase_detector(
+			input				rst_n							,
+			input				clk								,
+			input				Phase_in						,
+		   output  reg       Phase_valid,
+			output reg	[31:0]	Phase_cnt_out =1'b0
+		);
+			reg		[31:0]	Phase_cnt 	= 1'b0		;
+			reg		[2:0]	state 		= 1'b0		;//״̬��
+			reg				Phase_pos 	= 1'b0		;//�����ؼĴ���
+			reg				Phase_inreg = 1'b0		;
+
+			reg		[9:0]	count_high = 1'b0;
+			//����
+			
+			
+			reg		[99:0] Phase_in_shiftreg;
+			always@(posedge clk)
+				Phase_in_shiftreg <= {Phase_in_shiftreg[98:0],Phase_in};
+				
+/* 			parameter SAMPLE_TIME = 240;//������ȥ��240������----6us
+			reg		Phase_in_reg,Phase_in_reg1,Phase_in_reg2,Phase_in_filter = 1'b0;
+			
+			always @(posedge clk)
+				if(Phase_in ==1'b1 && (count_high < SAMPLE_TIME))
+				count_high <= count_high + 1;
+				else
+				count_high <= 0;
+			always @(posedge clk)
+				if(count_high == SAMPLE_TIME)
+				Phase_in_filter  <= Phase_in;
+				else if(Phase_in ==1'b0)
+				Phase_in_filter <= 1'b0;
+				else
+				Phase_in_filter <= Phase_in_filter;	 */
+				
+/* 			
+			always@(posedge clk)
+			begin
+				Phase_in_reg <= Phase_in;
+				Phase_in_reg1 <= Phase_in_reg;
+				Phase_in_reg2 <= Phase_in_reg1;
+				Phase_in_filter <= Phase_in | Phase_in_reg; end  */
+				
+	assign		Phase_inwire = &Phase_in_shiftreg		;		
+	always@(posedge clk) begin
+		Phase_inreg <= Phase_inwire	;
+		if(Phase_inwire == 1'b1 && Phase_inreg == 1'b0 )
+			Phase_pos <= 1'b1;
+		else
+			Phase_pos <= 1'b0; 
+	end
+	
+	always@(posedge clk,negedge rst_n) 
+		if(!rst_n) begin
+		   Phase_valid <= 'b0;
+			state 		<= 3'd0;
+			Phase_cnt	<= 1'b0; end
+		else begin
+		case(state)
+			3'd0: begin Phase_valid <= 'b0; if(Phase_pos) state <= 3'd1;else ;end
+			3'd1: begin
+			      Phase_valid  <= 'b0;
+					Phase_cnt <= Phase_cnt + 1'b1;
+					if(Phase_pos || (Phase_cnt >= 400000000))
+					state <= 3'd2;
+					else
+					state <=3'd1;
+					end
+			3'd2:	begin
+			      Phase_valid  <= 'b1;
+					if(Phase_cnt >= 400000000) begin
+					Phase_cnt_out 	<= 32'b0;
+					Phase_cnt 		<= 1'b0;
+					state  			<= 3'd1;end
+					else begin
+					Phase_cnt_out 	<= Phase_cnt;
+					Phase_cnt 		<= 1'b0;
+					state  			<= 3'd1; 
+					end
+					end
+			default:begin
+			      Phase_valid    <= 'b0;
+					state  			<= 3'd0;
+					Phase_cnt 		<= 1'b0; end
+			endcase
+			end
+	endmodule
+					
